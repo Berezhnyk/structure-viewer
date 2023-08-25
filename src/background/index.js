729d360fd182c631dd2921798629ea641c11a6c1
Ivan Berezhnyk <ivanberezhnyk@gmail.com>
2023-08-26 02:35:15 +0300

    Updated text and logos

--- src/background/index.js
+++ src/background/index.js
@@ -1,9 +1,3 @@
-chrome.runtime.onInstalled.addListener(function (object) {
-    if (object.reason === chrome.runtime.OnInstalledReason.INSTALL) {
-        chrome.tabs.create({ url: "https://clydedsouza.net" });
-    }
-});
-
 chrome.commands.onCommand.addListener((command) => {
     if(command === 'turn-on') {
         chrome.action.setBadgeText({ text: 'ON' });
@@ -13,10 +7,4 @@ chrome.commands.onCommand.addListener((command) => {
         chrome.action.setBadgeText({ text: 'OFF' });
         chrome.action.setBadgeBackgroundColor({ color: 'red' });
     }
-});
-
-chrome.omnibox.onInputEntered.addListener((text) => {
-    chrome.tabs.create({ url: 'https://facebook.com/' + encodeURIComponent(text) });
-    chrome.tabs.create({ url: 'https://twitter.com/' + encodeURIComponent(text) });
-    chrome.tabs.create({ url: 'https://instagram.com/' + encodeURIComponent(text) });
 });
