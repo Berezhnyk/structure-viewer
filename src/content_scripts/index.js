213de305c6eebc9ed57f3043242523fd596870bc
Ivan Berezhnyk <ivanberezhnyk@gmail.com>
2023-08-26 19:50:58 +0300

    Added code that handles JSON responses (streamed) and formatted it

--- src/content_scripts/index.js
+++ src/content_scripts/index.js
@@ -1,13 +1,63 @@
-import { getStorage } from "../common/storage";
-import { CHROME_SYNC_STORAGE_KEY, PRESET_CONFIGURATION } from "../common/settings";
+let accumulatedData = "";
+let updatingContent = false;
+let formattedJsonContainer;
 
-function loadAndDisplayStorageValue(result) {
-    const savedConfiguration = result || PRESET_CONFIGURATION;
-    const storageValue = savedConfiguration["storageValue"];    
-    console.info("Storage value is", storageValue);
+const observer = new MutationObserver((mutations) => {
+    for (const mutation of mutations) {
+        
+        if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+            for (const addedNode of mutation.addedNodes) {
+                if (addedNode.nodeName === "PRE" && addedNode.id !== 'formattedJsonContainer') {
+                    addedNode.style.display = 'none';
+                }
+            }
+        }
+
+        if (mutation.type === 'characterData' && !updatingContent) {
+            let node = mutation.target;
+            accumulatedData = node.nodeValue;
+
+            try {
+                accumulatedData = makeSureItIsJson(accumulatedData);
+                const jsonData = JSON.parse(accumulatedData);
+                const formattedData = formatJSON(jsonData);
+
+                // If the formattedJsonContainer with the specific ID doesn't exist yet, create it
+                if (!formattedJsonContainer) {
+                    formattedJsonContainer = document.createElement('pre');
+                    formattedJsonContainer.id = 'formattedJsonContainer';
+                    // Insert the new container at the beginning of the body
+                    document.body.insertBefore(formattedJsonContainer, document.body.firstChild);
+                }
+                
+                // Update the formattedJsonContainer with the formatted data
+                formattedJsonContainer.textContent = formattedData;
+                accumulatedData = "";
+
+            } catch (e) {
+                console.error("Current content is not valid JSON. Waiting for more data.");
+            }
+        }
+    }
+});
+
+function makeSureItIsJson(data) {
+    if (data.length === 0) {
+        return data;
+    }
+    if (data[0] === "{" && data[data.length - 1] !== "}") {
+        data += "}";
+    } else if (data[0] === "[" && data[data.length - 1] !== "]") {
+        data += "]";
+    }
+    return data;
 }
 
-window.onload = function() {
-    console.info("Content script loaded");
-    getStorage(CHROME_SYNC_STORAGE_KEY, loadAndDisplayStorageValue); 
+function formatJSON(data) {
+    return JSON.stringify(data, null, 2);
+}
+
+if (document.contentType === "application/json") {
+    const config = { childList: true, characterData: true, subtree: true };
+    observer.observe(document.documentElement, config);
 }
